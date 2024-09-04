405ddda983913cf4aa8d8af57d19c5b58f36628d
waqasahmed042 <waqasahmed.it042@gmail.com>
2024-09-04 09:58:10 +0500

    remove hard coded data

---
+++ textResource.js
@@ -0,0 +1,25 @@
+// Form Title
+const login_form = "Login Form";
+const signup_form = "Signup Form";
+
+document.getElementById("login_form").innerHTML = login_form;
+document.getElementById("signup_form").innerHTML = signup_form;
+
+// Form Buttons
+const login_button = "Login Form";
+const signup_button = "Signup Form";
+
+document.getElementById("login_button").innerHTML = login_button;
+document.getElementById("signup_button").innerHTML = signup_button;
+
+// Forgot Password
+const forgot_password = "Forgot password?";
+
+document.getElementById("forgot_password").innerHTML = forgot_password;
+
+// Not a Number & Signup now
+const not_a_member = "Not a member?";
+const signup_now = "Signup now";
+
+document.getElementById("not_a_member").innerHTML = not_a_member;
+document.getElementById("signup_now").innerHTML = signup_now;
