46069aad6a833f6821a6cd8864b5db478547cd0e
waqasahmed042 <waqasahmed.it042@gmail.com>
2024-09-04 11:36:35 +0500

    update login form

--- script.js
+++ script.js
@@ -4,14 +4,48 @@ const loginBtn = document.querySelector("label.login");
 const signupBtn = document.querySelector("label.signup");
 const signupLink = document.querySelector("form .signup-link a");
 
-signupBtn.onclick = (() => {
-    loginForm.style.marginLeft = "-50%";
-    loginText.style.marginLeft = "-50%";
-});
-
 loginBtn.onclick = (() => {
     loginForm.style.marginLeft = "0%";
     loginText.style.marginLeft = "0%";
+});
+
+function myFunction() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+
+    // Dummy credentials
+    const userEmail = "test@gmail.com";
+    const userPassword = "test1122$";
+
+    if (emailInput.value) {
+        event.preventDefault(); // Prevent form submission
+        if (emailInput.value === userEmail && passwordInput.value === userPassword) {
+            alert("Login successful!");
+            document.getElementById('email').value = "";
+            document.getElementById('password').value = "";
+        } else if (emailInput.value != userEmail && passwordInput.value === userPassword) {
+            alert("Invalid Email");
+        } else if (emailInput.value === userEmail && passwordInput.value != userPassword) {
+            alert("Invalid Password");
+        } else {
+            event.preventDefault(); // Prevent form submission
+            alert("Invalid email or password.");
+        }
+    }
+});
+
+signupBtn.onclick = (() => {
+    loginForm.style.marginLeft = "-50%";
+    loginText.style.marginLeft = "-50%";
 });
 
 signupLink.onclick = (() => {
--- textResource.js
+++ textResource.js
@@ -2,24 +2,24 @@
 const login_form = "Login Form";
 const signup_form = "Signup Form";
 
-document.getElementById("login_form").innerHTML = login_form;
-document.getElementById("signup_form").innerHTML = signup_form;
+document.getElementById("login_form").textContent = login_form;
+document.getElementById("signup_form").textContent = signup_form;
 
 // Form Buttons
 const login_button = "Login Form";
 const signup_button = "Signup Form";
 
-document.getElementById("login_button").innerHTML = login_button;
-document.getElementById("signup_button").innerHTML = signup_button;
+document.getElementById("login_button").textContent = login_button;
+document.getElementById("signup_button").textContent = signup_button;
 
 // Forgot Password
 const forgot_password = "Forgot password?";
 
-document.getElementById("forgot_password").innerHTML = forgot_password;
+document.getElementById("forgot_password").textContent = forgot_password;
 
 // Not a Number & Signup now
 const not_a_member = "Not a member?";
 const signup_now = "Signup now";
 
-document.getElementById("not_a_member").innerHTML = not_a_member;
-document.getElementById("signup_now").innerHTML = signup_now;
+document.getElementById("not_a_member").textContent = not_a_member;
+document.getElementById("signup_now").textContent = signup_now;
